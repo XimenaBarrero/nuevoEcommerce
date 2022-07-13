d30a05bcd35ee135e6c9ce41b47ea18e33a5abaa
CarmenXP <xocuaposadas.carmen@gmail.com>
2022-07-13 14:48:32 -0500

    shopping cart fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,56 +2,55 @@
 const menu = document.getElementById('btn-menu');
 const nav = document.getElementById('navegacion');
 const cerrar = document.getElementById('btn-close') 
-​
+
 const aHome = document.getElementById('a-home');
 const aProducts = document.getElementById('a-products');
-​
+
 const cartIcono = document.getElementById('cart-icono');
-​
+
 menu.addEventListener('click', ()=>{
     nav.style.transform = "translateX(0)";
     nav.style.transition =  "transform 800ms ease"; 
     cartIcono.style.zIndex = "100";
 });
-​
+
 const quitar = ()=>{ nav.style.transform = "translateX(100%)";};
-​
+
 cerrar.addEventListener('click', quitar);
 aHome.addEventListener('click', quitar);
 aProducts.addEventListener('click', quitar);
-​
+
 /* darktheme */
 const body = document.getElementById('body');
 const themeIcon = document.getElementById('theme-icon');
 const logo = document.getElementById('logo');
-​
+
 themeIcon.addEventListener('click', ()=>{
-​
+
     body.classList.toggle('darktheme')
-​
+
     let moon = body.classList.contains('darktheme')
     if (moon) {
         themeIcon.classList.replace('fa-moon', 'fa-sun')
     }else{
         themeIcon.classList.replace('fa-sun', 'fa-moon')
     }
-​
+
 });
-​
-​
+
 /* carrito */
-​
+
 const abrirCarrito = document.getElementById('cart-icono');
 const carrito = document.getElementById('carrito');
 const cerrar_carrito = document.getElementById('btn-close-cart');
-​
+
 abrirCarrito.addEventListener('click', ()=>{
     carrito.style.transform = "translateX(0)";
     carrito.style.transition =  "all 800ms ease";
 })
-​
+
 cerrar_carrito.addEventListener('click', ()=>{carrito.style.transform = "translateX(100%)";})
-​
+
 /* FILTRAR PRODUCTOS*/
 const productos = document.getElementById('section-productos'); /* CONTENEDOR DE LOS PRODUCTOS A FILTRAR */
 /* CADA BOTON DE FILTRADO */
@@ -61,10 +60,10 @@ const liShirts = document.getElementById('shirts');
 const liSweatshirts = document.getElementById('sweatshirts');
 /* CADA CONTENEDOR DE CADA PRODUCTO */
 const productosContainers = productos.children;
-​
+
 /* FILTRADO DE PRODUCTOS: A cada li donde están las opciones de filtrado les ponemos un data con el nombre
 de c/pruducto y a los container de cada producto también les ponemos un data con el nombre que corresponda */
-​
+
     /* Obtenemos cada contenedor de cada producto que tenga la class .noSelected y si la tiene se la quitamos,
     esto es para resetear los elementos cada vez que se hace un filtrado (esta clase tiene display:none)*/
     function mostrarEliminados(){
@@ -86,7 +85,7 @@ de c/pruducto y a los container de cada producto también les ponemos un data co
     liHoodies.addEventListener('click', filtrarOpcion); /* li data hoodie */
     liShirts.addEventListener('click', filtrarOpcion); /* li data shirt */
     liSweatshirts.addEventListener('click', filtrarOpcion); /* li data sweatshirt */
-​
+
 /* OBJETOS CON LA INFO DE CADA PRODUCTO QUE IRÁN CAMBIANDO MIENTRAS AVANCE EL PROGRAMA */
     const products = [{
         nombre: "hoodie",
@@ -112,15 +111,14 @@ de c/pruducto y a los container de cada producto también les ponemos un data co
         subtotal: 0
         /* imagen: 'img/featured3.png' */
     }]
-​
-​
+
 /* el div que mostramos cuando no hay ningún producto y el div que mostramos cuando añadimos productos al cart */
 emptyCarrito = document.getElementById('empty-carrito');
 carritoProducts = document.getElementById('carrito-products');
 /* inicializamos en falso el estado del div donde se muestran los productos porque este solo se muestra
 cuando añadimos algún producto*/
 let estadoCart = false;
-​
+
 /* QUITAR EL DIV DE CARRITO VACIO Y PONER EL DIV DE PRODUCTOS Y VICESERVA, dependiendo del estado actual del cart*/
 const mostrarCarritoProducts = ()=>{
     if (estadoCart == false) {
@@ -133,14 +131,14 @@ const mostrarCarritoProducts = ()=>{
         estadoCart = false;
     }
 };
-​
+
 /* Declaramos variables para el total de unidades de los productos (suma de cantidad) seleccionados y su precio total
 (suma de subtotales), esto lo obtendremos al manipular los objetos de cada producto*/
 let unidadesTotales;
 let precioTotal;
 /* CONTENEDOR DONDE ESTÁN LAS UNIDADES TOTALES Y EL PRECIO TOTAL */
 const itemsPrecio = document.getElementById('items-precios');
-​
+
 /* SUMAMOS LAS UNIDADES, como parámetros tenemos el contenedor que queremos agregar y el data, osea el nombre del producto */
 const sumarUnidades = (element,product) => {
     /* obtenemos el container donde está el texto a cambiar (texto-product), en este están los datos del producto*/
@@ -166,13 +164,13 @@ const sumarUnidades = (element,product) => {
     itemsPrecio.firstElementChild.textContent = `${unidadesTotales} items`;
     itemsPrecio.lastElementChild.textContent = `$${precioTotal}.00`;
 }
-​
+
 /* MOSTRAR EN EL DIV DEL CARRITO LOS PRODUCTOS SELECCIONADOS */
 const carritoContainers = document.querySelectorAll('.carrito-container'); /* Lista de cada contenedor de product */
 const contCarrito = document.getElementById('cont-carrito'); /* contador del icono de carrito en la nav */
     let contadorCarrito = 0; /* variable que será la que asignemos al contador del icono de carrito */
 const checkout = document.getElementById('checkout'); /* boton para realizar la compra */
-​
+
 const añadirElemento = event => {
     if (estadoCart==false) { /* si no está activa el div de los productos mostrarlo y activar el boton de compra */
         mostrarCarritoProducts();
@@ -192,7 +190,7 @@ const añadirElemento = event => {
     for (const x of carritoContainers) { /* recorremos cada contenedor de productos */
     /* si su data y la del producto que queremos agregar son iguales entonces lo muestra al quitarle el
     noSelected, porque pusimos que todos así (osea que no se muestren)) */
-​
+
         if(x.dataset.cartproduct == productoActual){ 
             x.classList.remove('noSelected');
             sumarUnidades(x, productoActual); /* le pasamos el contenedor del producto y el data del btn que coincide */
@@ -208,12 +206,12 @@ btnSweatshirt = document.getElementById('btn-sweatshirt');
 btnHoodie.addEventListener('click', añadirElemento);
 btnShirt.addEventListener('click', añadirElemento);
 btnSweatshirt.addEventListener('click', añadirElemento);
-​
+
 /* BOTONES MÁS, MENOS Y ELIMINAR */
 const btnMenos = document.querySelectorAll('.btn-menos');
 const btnMas = document.querySelectorAll('.btn-mas');
 const btnBorrar = document.querySelectorAll('.btn-borrar');
-​
+
 const operar = event =>{
     /* obtenemos la data de los botones para saber qué producto y acción es.
     Podemos saber estas 2 cosas porque el formato es (ejem: data='restar-hoodie') */
@@ -226,11 +224,11 @@ const operar = event =>{
     const subTotal = event.target.parentNode.previousElementSibling;
     /* accedemos al padre del padre del padre jaja que es el carrito-container*/
     const padreContainer = event.target.parentNode.parentNode.parentNode;
-​
+
     /* quitamos el restar- y sumar- por conflicto con nombre (shirt), porque
     el sweatshirt se sumaba 2 veces al contener la palabra shirt también */
         let productoFinal = productoActual.slice(productoActual.indexOf('-')+1);
-​
+
         for (const x in products) { /* recorremos los objetos */
          if (productoFinal == products[x].nombre) { /* si coincide la data con el nombre del objeto */
              if (productoActual.includes('restar')) {
@@ -260,5 +258,86 @@ const operar = event =>{
                 contCarrito.textContent = contadorCarrito;
                 units.textContent = `${products[x].cantidad} units`;
                 subTotal.textContent = `Subtotal: $${products[x].subtotal}.00`
-             } else{ /* operación eliminar */
-                /* quitamos el producto agregando la clase de dis...
+                }else{ /* operación eliminar */
+                /* quitamos el producto agregando la clase de display none*/
+                padreContainer.classList.add('noSelected');
+                /* restamos la cantidad actual de unidades en el ícono del carrito*/
+                contadorCarrito-=products[x].cantidad;
+                contCarrito.textContent = contadorCarrito;
+                products[x].cantidad=0; /* devolvemos los valores del objeto a 0*/
+                products[x].subtotal=0;
+             }
+             /* luego de realizar c/operacion, sumamos las unidades y subtotales de c/u
+             para obtener el total de unidades y el precio total */
+            unidadesTotales = products.map(item => item.cantidad).reduce((prev, curr) => prev + curr, 0)
+            precioTotal = products.map(item => item.subtotal).reduce((prev, curr) => prev + curr, 0)
+             /* mostramos en el html estos valores */
+            itemsPrecio.firstElementChild.textContent = `${unidadesTotales} items`;
+            itemsPrecio.lastElementChild.textContent = `$${precioTotal}.00`;
+            /* si ya no hay productos que mostrar, entonces volvemos al
+            div del empty-card y ocultamos este div de productos */
+            if(unidadesTotales==0){
+                mostrarCarritoProducts();
+                checkout.classList.remove('btn-checkout'); /* desactivamos el boton */
+            }
+         }      
+        }  
+};
+/* recorremos todos los botones de sumar restar y eliminar y a todos
+les aplicamos el evento listener al hacerles click para que operen*/
+btnMenos.forEach(x=>x.addEventListener('click', operar));
+btnMas.forEach(x=>x.addEventListener('click', operar));
+btnBorrar.forEach(x=>x.addEventListener('click', operar));
+
+/* FUNCION AL PRESIONAR EL BOTON DE CHECKOUT, OSEA AL SIMULAR LA COMPRA */
+const resetearCarrito = event =>{
+    /* reseteamos el contador y el icono de carrito */
+    contadorCarrito=0;
+    contCarrito.textContent = contadorCarrito;
+
+    let i = 0; /* contador para los objetos, para que se reinicie a la par de c/container */
+    for (const x of carritoContainers) { /* container de c/producto */
+        x.classList.add('noSelected'); /* display none al contenedor */
+        /* Accedemos a las unidades y subTotal del html*/
+        const infoProducto = x.lastElementChild;
+        const unidades = infoProducto.lastElementChild;
+        const unitsProducto = unidades.children[1];
+        const subTotalProducto = infoProducto.children[2];
+        /* restamos la cantidad al stock*/ 
+        products[i].stock-=products[i].cantidad;
+        /* Accedemos y cambiamos el stock del html */
+        const stock = infoProducto.children[1].firstElementChild;
+        stock.textContent = `Stock: ${products[i].stock}`;
+        /* valores de objetos a 0 y lo mostramos en el html */
+        products[i].cantidad=0;
+        products[i].subtotal=0;
+        unitsProducto.textContent = `${products[i].cantidad} units`;
+        subTotalProducto.textContent = `Subtotal: $${products[i].subtotal}.00`
+        
+        /* cambiamos los stock del filtrado (productosContainer son los container de cada product) */
+        for (const y of productosContainers) { /* Recorremos cada product container */
+            if(y.dataset.product == products[i].nombre) {
+                const stockFiltro = y.lastElementChild.children[1];
+                stockFiltro.textContent = `Stock: ${products[i].stock}`;
+            }
+        }
+        i++;
+    }
+    
+    /* PONEMOS LOS VALORES GENERALES EN 0 */
+    unidadesTotales = products.map(item => item.cantidad).reduce((prev, curr) => prev + curr, 0)
+    precioTotal = products.map(item => item.subtotal).reduce((prev, curr) => prev + curr, 0)
+    itemsPrecio.firstElementChild.textContent = `${unidadesTotales} items`;
+    itemsPrecio.lastElementChild.textContent = `$${precioTotal}.00`;
+
+    /* OCULTAMOS EL CARRITO, DESACTIVAMOS BOTON Y MOSTRAMOS EL EMPTY */
+    mostrarCarritoProducts();
+    checkout.classList.remove('btn-checkout');
+    alert('Compra realizada con éxito');
+}
+
+checkout.addEventListener('click', resetearCarrito);
+
+/* EXTRAS */
+
+
